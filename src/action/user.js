8eabe56574bed0ffcc6f84cec2e727588b1ca452
ALDO FERDIANSYAH - PARTNER CST <cst_dev36@unitedtractors.com>
2020-09-11 22:28:40 +0700

    adding actions redux for services user

--- src/action/user.js
+++ src/action/user.js
@@ -1,4 +1,4 @@
-import {GET_USER, SUCCESS_TYPE, FAILURE_TYPE} from 'action/actionTypes';
+import {GET_USER, SUCCESS_TYPE, FAILURE_TYPE, REQUEST_TYPE} from 'action/actionTypes';
 import {getStorage} from 'utils/storage.helper';
 export const getUser = () => dispatch => {
     dispatch({type:`${GET_USER}${REQUEST_TYPE}`});
@@ -15,3 +15,66 @@ export const getUser = () => dispatch => {
         });
     }  
 };
+
+export const listUser = (data,token) => dispatch => {
+    dispatch({
+        type:`${GET_USER}${REQUEST_TYPE}`
+    })
+    try {
+        dispatch({
+            type:`${GET_USER}${SUCCESS_TYPE}`
+        })
+    } catch (error) {
+        type:`${GET_USER}${FAILURE_TYPE}`
+    }
+}
+export const createUser = (data,token) => dispatch => {
+    dispatch({
+        type:`${CREATE_USER}${REQUEST_TYPE}`
+    })
+    try {
+        dispatch({
+            type:`${CREATE_USER}${SUCCESS_TYPE}`
+        })
+    } catch (error) {
+        type:`${CREATE_USER}${FAILURE_TYPE}`
+    }
+}
+export const getUserDetail = (data,token) => dispatch => {
+    dispatch({
+        type:`${DETAIL_USER}${REQUEST_TYPE}`
+    })
+    try {
+        dispatch({
+            type:`${DETAIL_USER}${SUCCESS_TYPE}`
+        })
+    } catch (error) {
+        dispatch({
+            type:`${DETAIL_USER}${FAILURE_TYPE}`
+        })
+    }
+}
+export const updateUser = (data,token) => dispatch => {
+    dispatch({
+        type:`${UPDATE_USER}${REQUEST_TYPE}`
+    })
+    try {
+        dispatch({
+            type:`${UPDATE_USER}${SUCCESS_TYPE}`
+        })
+    } catch (error) {
+        type:`${UPDATE_USER}${FAILURE_TYPE}`
+    }
+}
+export const deleteUser = (data,token) => dispatch => {
+    dispatch({
+        type:`${DELETE_USER}${REQUEST_TYPE}`
+    })
+    try {
+        dispatch({
+            type:`${DELETE_USER}${SUCCESS_TYPE}`
+        })
+    } catch (error) {
+        type:`${DELETE_USER}${FAILURE_TYPE}`
+    }
+}
